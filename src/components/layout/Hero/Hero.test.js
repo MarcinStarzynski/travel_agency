db973a92a77bd412e486df526a154daa4d55d8c9
Marcin Starzynski <gienekmsms@gmail.com>
2021-03-17 18:19:35 +0100

    Add Trip Summary Tests

---
+++ src/components/layout/Hero/Hero.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Hero from './Hero';
+
+describe('Component Hero', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<Hero titleText='Lorem ipsum' imageSrc='dowolna wartość tekstowa' />);
+    expect(component).toBeTruthy();
+    console.log(component.debug());
+  });
+
+  it('should throw error without required props', () => {
+    expect(() => shallow(<Hero />)).toThrow();
+  });
+
+  it('should render correct title and imageconst', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const expectedImage = 'ladny_obrazek.png';
+    const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage}/>);
+
+    const renderedTitle = component.find('.title').text();
+    const renderedImage = component.find('.image').prop('src');
+
+    expect(renderedTitle).toEqual(expectedTitle);
+    expect(renderedImage).toEqual(expectedImage);
+  });
+
+  it('renders correct classNames', () => {
+    const mockVariants = 'small dummy';
+    const component = shallow(<Hero titleText='Lorem' imageSrc='Obrazek.png' variant={mockVariants} />);
+
+    expect(component.hasClass('component')).toBe(true);
+    expect(component.hasClass('small')).toBe(true);
+    expect(component.hasClass('dummy')).toBe(true);
+  });
+});
